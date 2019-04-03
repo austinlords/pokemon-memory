ec624813772a04284fc73b5140e9fbe3d5bb6f51
Austin Lords <austinlords@outlook.com>
2019-04-03 14:47:54 -0700

    flip card functionality added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//back-end logic
-var pokemon = ["charmander", "gengar", "oddish", "onix", "pikachu", "koffing", "snorlax", "training-cards", "alakazam", "articuno", "ash", "gameboy", "gym-leader", "safari-ball", "sandslash"]
-
+// back-end logic
+const pokemon = ['alakazam', 'articuno', 'ash', 'charmander', 'gameboy', 'gengar', 'gym-leader', 'koffing', 'oddish', 'onix', 'pikachu', 'safari-ball', 'sandslash', 'snorlax', 'training-cards'];
 var clickCount = 0;
 var flipCount = 0;
 var pairsFound = 0;
@@ -8,132 +7,123 @@ var sec = 0;
 var min = 0;
 var imgOne, imgTwo;
 
-function prepareGame(array) {
-	array = duplicateArray(array);
-	array = shuffleArray(array);
-	addImages(array);
-	hideImages(array);
-	playGame(array);
-};
-
 function duplicateArray(array) {
-	array.forEach(function(element) {
+  array.forEach((element) => {
 		array.push(element);
 	});
-	return array;
-};
+  return array;
+}
 
 function shuffleArray(array) {
-	var i = array.length;
+  let i = array.length;
   while (i > 0) {
-		var r = Math.floor(Math.random() * i);
-		array.push(array[r]);
-		array.splice(r, 1);
-		i--;
+    let r = Math.floor(Math.random() * i);
+    array.push(array[r]);
+    array.splice(r, 1);
+    i--;
   } return array;
 }
 
 function addImages(array) {
-	array.forEach(function(element, index) {
-		$(".col-2:nth-of-type("+ (index + 1) +")").append("<img src=images/" + element + ".png class='pokePics img-fluid'>");
+  array.forEach((element, index) => {
+		$('#card'+ (index + 1)).append(`<img src=images/${element}.png class='pokePics img-fluid'>`);
 	});
-};
+}
 
-function hideImages(array) {
-	$('.pokePics').hide();
-};
-
-function playGame(array) {
-	$(".col-2").click(function() {
-		if (flipCount < 2) {
-			if (clickCount === 0) {
-				startTimer();
-			}
-			activePic = $(this).children('.pokePics');
-			if (flipCount === 0 && !activePic.hasClass("matched")) {
-				imgOne = showImage(activePic, 'first');
-				++flipCount;
-				++clickCount
-			}	else if (flipCount === 1  && !activePic.is('.first, .matched')) {
-				imgTwo = showImage(activePic, 'second');
-				++flipCount;
-				++clickCount;
-			}
-			if (flipCount === 2) {
-				var match = checkMatch(imgOne,imgTwo);
-				if (match) {
-					matchFound();
-					if (pairsFound < 15) {
-						reset();
-					} else youWon();
-				} else {
-					setTimeout(matchNotFound, 2000);
-				}
-			}
-			$('#movesCounter').html(clickCount);
-		}
-	});
-};
-
-
-function showImage(image, imgClass) {
-	image.show();
-	image.toggleClass(imgClass);
-	image.siblings().hide();
+function showImage(image, cls) {
+	image.parent().parent().toggleClass('flip');
+	image.toggleClass(cls);
 	img = image.attr('src');
-	return img
+  return img;
 }
 
 function checkMatch(imgOne,imgTwo) {
 	if (imgOne === imgTwo) return true;
-	else return false;
+	return false;
 }
 
-function matchFound(imgOne,imgTwo) {
-	$('.first').toggleClass('matched first');
-	$('.second').toggleClass('matched second');
-	++pairsFound;
-	console.log(pairsFound);
-}
-
-function matchNotFound(imgOne,imgTwo) {
-	$('.first').hide();
-	$('.first').siblings().show();
-	$('.second').hide();
-	$('.second').siblings().show();
+function matchFound() {
+	$('.first, .second').toggleClass('matched');
+	$('.first, .second').parent().toggleClass('shadow');
 	$('.first').toggleClass('first');
-	$('.second').toggleClass('second');
-	reset();
+  $('.second').toggleClass('second');
+  ++pairsFound;
+}
+
+function matchNotFound() {
+	$('.first, .second').parent().parent().toggleClass('flip');
+  $('.first').toggleClass('first');
+  $('.second').toggleClass('second');
+  reset();
 }
 
 function startTimer() {
-	setInterval(runTimer, 1000);
-};
+  setInterval(runTimer, 1000);
+}
 
 function runTimer() {
-	if (sec != 59) {
-			++sec;
-		} else if (sec = 59) {
-			++min;
-			sec = 0;
-		}
-	$("#seconds").html(sec);
-	$("#minutes").html(min);
-};
+  if (sec != 59) {
+    ++sec;
+  } else if (sec = 59) {
+    ++min;
+    sec = 0;
+  }
+  $('#seconds').html(sec);
+  $('#minutes').html(min);
+}
 
 function reset() {
-	imgOne = undefined;
-	imgTwo = undefined;
-	flipCount = 0;
+  imgOne = undefined;
+  imgTwo = undefined;
+  flipCount = 0;
 }
 
 function youWon() {
-	alert('CONGRATULATIONS! You won in ' + clickCount + ' moves!')
-}
+  alert(`CONGRATULATIONS! You won in ${clickCount} moves!`);
+} 
 
 
-//front-end logic
-$(function() {
+// front-end logic
+$(() => {
+
+	function prepareGame(array) {
+		array = duplicateArray(array);
+		array = shuffleArray(array);
+		addImages(array);
+		playGame(array);
+	};
+
+	function playGame(array) {
+		$('.card-front').click(function() {
+			if (flipCount < 2) {
+				if (clickCount === 0) {
+					startTimer();
+				}
+				activePic = $(this).siblings('.card-back').children();
+				if (flipCount === 0 && !activePic.hasClass('matched')) {
+					imgOne = showImage(activePic, 'first');
+					++flipCount;
+					++clickCount
+				}	else if (flipCount === 1  && !activePic.is('.first, .matched')) {
+					imgTwo = showImage(activePic, 'second');
+					++flipCount;
+					++clickCount;
+				}
+				$('#movesCounter').html(clickCount);
+				if (flipCount === 2) {
+					var match = checkMatch(imgOne,imgTwo);
+					if (match) {
+						matchFound();
+						if (pairsFound < 15) {
+							reset();
+						} else youWon();
+					} else {
+						setTimeout(matchNotFound, 1200);
+					}
+				}
+			}
+		});
+	};
 
 	prepareGame(pokemon);
 
